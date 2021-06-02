cc5ba0ed20a34d90af6e94bf95e89705f8dd763f
hameed <40880601+hamsyed@users.noreply.github.com>
2021-06-02 18:13:42 +1000

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "hellow-world" {
  ami = "${var.ami}" 
  instance_type = "${var.instance_type}"
+ iam_instance_profile = "${aws_iam_instance_profile.ec2_profile.name}"	
  vpc_security_group_ids = ["${aws_security_group.allow_jenkins.id}"]
  availability_zone = "${var.azs}"
  key_name = "terraform"
